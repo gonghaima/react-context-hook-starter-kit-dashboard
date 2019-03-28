a6d216fcbce318a858d55a82ef23ebe0df530591
Steven <gonghaima@hotmail.com>
2019-03-28 20:29:57 +1100

    start working on header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Store from "./store";
 
 // Load components
 import Form from "./components/Form";
+import Header from "./components/Header";
 
 export default () => {
   return (
     <Store>
+      <Header />
       <div className="App">
         <Form />
       </div>
