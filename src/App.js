c4653ad6a2a07b28de70e433add3349220d1b619
Steven <gonghaima@hotmail.com>
2019-03-28 20:51:07 +1100

    start adding subheader section

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Store from "./store";
 // Load components
 import Form from "./components/Form";
 import Header from "./components/Header";
+import SubHeader from "./components/SubHeader";
 
 export default () => {
   return (
     <Store>
       <Header />
+      <SubHeader />
       <div className="App">
         <Form />
       </div>
---
+++ src/components/SubHeader/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./SubHeader.module.css";
+
+const SubHeader = () => {
+  return (
+    <div className={styles.subheader}>
+      <p>Subheader content</p>
+    </div>
+  );
+};
+
+export default SubHeader;
