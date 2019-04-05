26de79c6581642dff37d6beb5d9ab70c0e753ef1
Steven <gonghaima@hotmail.com>
2019-04-05 21:03:29 +1100

    use context.consumer instead of direct import context

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -4,16 +4,33 @@ import Search from "../Search";
 import ContentItem from "../ContentItem";
 import { StoreContext } from "../../store";
 
-export default () => {
-  const [{ users }] = useContext(StoreContext);
+// export default () => {
+//   const [{ users }] = useContext(StoreContext);
 
+//   return (
+//     <div className={styles.contentWrapper}>
+//       <div className={styles.contentItemWrapper}>
+//         <Search />
+//         {users &&
+//           users.map((user, key) => <ContentItem key={key} user={user} />)}
+//       </div>
+//     </div>
+//   );
+// };
+export default () => {
   return (
-    <div className={styles.contentWrapper}>
-      <div className={styles.contentItemWrapper}>
-        <Search />
-        {users &&
-          users.map((user, key) => <ContentItem key={key} user={user} />)}
-      </div>
-    </div>
+    <StoreContext.Consumer>
+      {([state]) => (
+        <div className={styles.contentWrapper}>
+          <div className={styles.contentItemWrapper}>
+            <Search />
+            {state.users &&
+              state.users.map((user, key) => (
+                <ContentItem key={key} user={user} />
+              ))}
+          </div>
+        </div>
+      )}
+    </StoreContext.Consumer>
   );
 };
