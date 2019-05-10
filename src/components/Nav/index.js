63d625a3f063973ff019c91f58fdfe3adcaa9125
Steven <gonghaima@hotmail.com>
2019-05-10 14:48:46 +1000

    programatically get [state,dispatch] from context, instead of context.Consumer

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,109 +4,95 @@ import { Star, VerifiedUser, People, AccountCircle, Inbox } from "../../icons";
 import { StoreContext } from "../../store";
 
 export default () => {
+  const [state, dispatch] = React.useContext(StoreContext);
+  const sendFilter = (e, tp) => {
+    e.preventDefault();
+    return dispatch({
+      type: "setFilter",
+      payload: tp
+    });
+  };
   return (
-    <StoreContext.Consumer>
-      {([state, dispatch]) => {
-        const sendFilter = (e, tp) => {
-          e.preventDefault();
-          return dispatch({
-            type: "setFilter",
-            payload: tp
-          });
-        };
-        return (
-          <div className={`${styles.nav} ${state.darkMode ? styles.dark : ""}`}>
-            <div className={styles.itemWrapper}>
-              <ul className={styles.itemUl}>
-                <li
-                  className={`${
-                    state.selected.queryValue === "all" ? styles.selected : ""
-                  }`}
-                >
-                  <a href="all" onClick={e => sendFilter(e, "all")}>
-                    <div>
-                      <AccountCircle />
-                    </div>
-                    <div>
-                      <span>All Users</span>
-                    </div>
-                  </a>
-                </li>
-                <li
-                  className={`${
-                    state.selected.queryValue === "favorites"
-                      ? styles.selected
-                      : ""
-                  }`}
-                >
-                  <a href="all" onClick={e => sendFilter(e, "favorites")}>
-                    <div>
-                      <Star />
-                    </div>
-                    <div>
-                      <span>Favorites</span>
-                    </div>
-                  </a>
-                </li>
-                <li
-                  className={`${
-                    state.selected.queryValue === "administrator"
-                      ? styles.selected
-                      : ""
-                  }`}
-                >
-                  <a
-                    href="administrator"
-                    onClick={e => sendFilter(e, "administrator")}
-                  >
-                    <div>
-                      <VerifiedUser />
-                    </div>
-                    <div>
-                      <span>Administrator</span>
-                    </div>
-                  </a>
-                </li>
-                <li
-                  className={`${
-                    state.selected.queryValue === "nonAdmins"
-                      ? styles.selected
-                      : ""
-                  }`}
-                >
-                  <a
-                    href="none-Admins"
-                    onClick={e => sendFilter(e, "none-Admins")}
-                  >
-                    <div>
-                      <People />
-                    </div>
-                    <div>
-                      <span>None-Admins</span>
-                    </div>
-                  </a>
-                </li>
-                <li
-                  className={`${
-                    state.selected.queryValue === "archived"
-                      ? styles.selected
-                      : ""
-                  }`}
-                >
-                  <a href="archived" onClick={e => sendFilter(e, "archived")}>
-                    <div>
-                      <Inbox />
-                    </div>
-                    <div>
-                      <span>Archived</span>
-                    </div>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        );
-      }}
-    </StoreContext.Consumer>
+    <div className={`${styles.nav} ${state.darkMode ? styles.dark : ""}`}>
+      <div className={styles.itemWrapper}>
+        <ul className={styles.itemUl}>
+          <li
+            className={`${
+              state.selected.queryValue === "all" ? styles.selected : ""
+            }`}
+          >
+            <a href="all" onClick={e => sendFilter(e, "all")}>
+              <div>
+                <AccountCircle />
+              </div>
+              <div>
+                <span>All Users</span>
+              </div>
+            </a>
+          </li>
+          <li
+            className={`${
+              state.selected.queryValue === "favorites" ? styles.selected : ""
+            }`}
+          >
+            <a href="all" onClick={e => sendFilter(e, "favorites")}>
+              <div>
+                <Star />
+              </div>
+              <div>
+                <span>Favorites</span>
+              </div>
+            </a>
+          </li>
+          <li
+            className={`${
+              state.selected.queryValue === "administrator"
+                ? styles.selected
+                : ""
+            }`}
+          >
+            <a
+              href="administrator"
+              onClick={e => sendFilter(e, "administrator")}
+            >
+              <div>
+                <VerifiedUser />
+              </div>
+              <div>
+                <span>Administrator</span>
+              </div>
+            </a>
+          </li>
+          <li
+            className={`${
+              state.selected.queryValue === "nonAdmins" ? styles.selected : ""
+            }`}
+          >
+            <a href="none-Admins" onClick={e => sendFilter(e, "none-Admins")}>
+              <div>
+                <People />
+              </div>
+              <div>
+                <span>None-Admins</span>
+              </div>
+            </a>
+          </li>
+          <li
+            className={`${
+              state.selected.queryValue === "archived" ? styles.selected : ""
+            }`}
+          >
+            <a href="archived" onClick={e => sendFilter(e, "archived")}>
+              <div>
+                <Inbox />
+              </div>
+              <div>
+                <span>Archived</span>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
