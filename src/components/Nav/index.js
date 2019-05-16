486763290d88ba868de92d70e6a275aeeb802e8f
Steven <gonghaima@hotmail.com>
2019-05-16 16:14:50 +1000

    animation improvement

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,96 +4,108 @@ import styles from "./Nav.module.scss";
 import { Star, VerifiedUser, People, AccountCircle, Inbox } from "../../icons";
 import { StoreContext } from "../../store";
 
-export default () => {
-  const [state, dispatch] = React.useContext(StoreContext);
-  const sendFilter = (e, tp) => {
-    // e.preventDefault();
-    return dispatch({
-      type: "setFilter",
-      payload: tp
-    });
-  };
-  return (
-    <div className={`${styles.nav} ${state.darkMode ? styles.dark : ""}`}>
-      <div className={styles.itemWrapper}>
-        <ul className={styles.itemUl}>
-          <li
-            className={`${
-              state.selected.queryValue === "all" ? styles.selected : ""
-            }`}
-          >
-            <Link to="all" onClick={e => sendFilter(e, "all")}>
-              <div>
-                <AccountCircle />
-              </div>
-              <div>
-                <span>All Users</span>
-              </div>
-            </Link>
-          </li>
-          <li
-            className={`${
-              state.selected.queryValue === "favorites" ? styles.selected : ""
-            }`}
-          >
-            <Link to="favorites" onClick={e => sendFilter(e, "favorites")}>
-              <div>
-                <Star />
-              </div>
-              <div>
-                <span>Favorites</span>
-              </div>
-            </Link>
-          </li>
-          <li
-            className={`${
-              state.selected.queryValue === "administrator"
-                ? styles.selected
-                : ""
-            }`}
-          >
-            <Link
-              to="administrator"
-              onClick={e => sendFilter(e, "administrator")}
-            >
-              <div>
-                <VerifiedUser />
-              </div>
-              <div>
-                <span>Administrator</span>
-              </div>
-            </Link>
-          </li>
-          <li
-            className={`${
-              state.selected.queryValue === "nonAdmins" ? styles.selected : ""
-            }`}
-          >
-            <Link to="nonAdmins" onClick={e => sendFilter(e, "none-Admins")}>
-              <div>
-                <People />
-              </div>
-              <div>
-                <span>None-Admins</span>
-              </div>
-            </Link>
-          </li>
-          <li
-            className={`${
-              state.selected.queryValue === "archived" ? styles.selected : ""
-            }`}
-          >
-            <Link to="archived" onClick={e => sendFilter(e, "archived")}>
-              <div>
-                <Inbox />
-              </div>
-              <div>
-                <span>Archived</span>
-              </div>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+export default () => (
+  <StoreContext.Consumer>
+    {([state, dispatch]) => {
+      const sendFilter = (e, tp) => {
+        // e.preventDefault();
+        return dispatch({
+          type: "setFilter",
+          payload: tp
+        });
+      };
+      return (
+        <div className={`${styles.nav} ${state.darkMode ? styles.dark : ""}`}>
+          <div className={styles.itemWrapper}>
+            <ul className={styles.itemUl}>
+              <li
+                className={`${
+                  state.selected.queryValue === "all" ? styles.selected : ""
+                }`}
+              >
+                <Link to="all" onClick={e => sendFilter(e, "all")}>
+                  <div>
+                    <AccountCircle />
+                  </div>
+                  <div>
+                    <span>All Users</span>
+                  </div>
+                </Link>
+              </li>
+              <li
+                className={`${
+                  state.selected.queryValue === "favorites"
+                    ? styles.selected
+                    : ""
+                }`}
+              >
+                <Link to="favorites" onClick={e => sendFilter(e, "favorites")}>
+                  <div>
+                    <Star />
+                  </div>
+                  <div>
+                    <span>Favorites</span>
+                  </div>
+                </Link>
+              </li>
+              <li
+                className={`${
+                  state.selected.queryValue === "administrator"
+                    ? styles.selected
+                    : ""
+                }`}
+              >
+                <Link
+                  to="administrator"
+                  onClick={e => sendFilter(e, "administrator")}
+                >
+                  <div>
+                    <VerifiedUser />
+                  </div>
+                  <div>
+                    <span>Administrator</span>
+                  </div>
+                </Link>
+              </li>
+              <li
+                className={`${
+                  state.selected.queryValue === "nonAdmins"
+                    ? styles.selected
+                    : ""
+                }`}
+              >
+                <Link
+                  to="nonAdmins"
+                  onClick={e => sendFilter(e, "none-Admins")}
+                >
+                  <div>
+                    <People />
+                  </div>
+                  <div>
+                    <span>None-Admins</span>
+                  </div>
+                </Link>
+              </li>
+              <li
+                className={`${
+                  state.selected.queryValue === "archived"
+                    ? styles.selected
+                    : ""
+                }`}
+              >
+                <Link to="archived" onClick={e => sendFilter(e, "archived")}>
+                  <div>
+                    <Inbox />
+                  </div>
+                  <div>
+                    <span>Archived</span>
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      );
+    }}
+  </StoreContext.Consumer>
+);
