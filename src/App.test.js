e656e964766cf31612453776fa617e8bd2687afe
Steven <gonghaima@hotmail.com>
2019-05-22 13:46:37 +1000

    improvement on unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import TestRenderer from "react-test-renderer";
 import App from "./App";
-
-const originalError = console.error;
-
-beforeAll(() => {
-  console.error = jest.fn();
-});
-
-afterAll(() => {
-  console.error = originalError;
-});
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const appComponent = TestRenderer.create(<App />, div);
+  expect(appComponent).toBeDefined();
 });
