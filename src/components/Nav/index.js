70a6114cfaf998fba1dc65e544bd8f0a6dfaa557
Steven <gonghaima@hotmail.com>
2019-05-25 15:30:12 +1000

    nav.js refactor, improved test coverage

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,60 +8,71 @@ export default props => {
   const history = props;
   const [state, dispatch] = React.useContext(StoreContext);
   const sendFilter = send(dispatch, history);
-  const generateAnchor = (children, sf, key, displayText) => (
-    <a href="/" onClick={e => sf(e, key)}>
-      <div>{children}</div>
-      <div>
-        <span>{displayText}</span>
-      </div>
-    </a>
+  const generateAnchor = (
+    children,
+    sf,
+    key,
+    displayText,
+    qVal,
+    styleSelected
+  ) => (
+    <li className={`${qVal === key ? styleSelected : ""}`}>
+      <a href="/" onClick={e => sf(e, key)}>
+        <div>{children}</div>
+        <div>
+          <span>{displayText}</span>
+        </div>
+      </a>
+    </li>
   );
   return (
     <div className={`${styles.nav} ${state.darkMode ? styles.dark : ""}`}>
       <div className={styles.itemWrapper}>
         <ul className={styles.itemUl}>
-          <li
-            className={`${
-              state.selected.queryValue === "all" ? styles.selected : ""
-            }`}
-          >
-            {generateAnchor(<AccountCircle />, sendFilter, "all", "All Users")}
-          </li>
-          <li
-            className={`${
-              state.selected.queryValue === "favorites" ? styles.selected : ""
-            }`}
-          >
-            {generateAnchor(<Star />, sendFilter, "favorites", "Favorites")}
-          </li>
-          <li
-            className={`${
-              state.selected.queryValue === "administrator"
-                ? styles.selected
-                : ""
-            }`}
-          >
-            {generateAnchor(
-              <VerifiedUser />,
-              sendFilter,
-              "administrator",
-              "Administrator"
-            )}
-          </li>
-          <li
-            className={`${
-              state.selected.queryValue === "nonAdmins" ? styles.selected : ""
-            }`}
-          >
-            {generateAnchor(<People />, sendFilter, "nonAdmins", "None-Admins")}
-          </li>
-          <li
-            className={`${
-              state.selected.queryValue === "archived" ? styles.selected : ""
-            }`}
-          >
-            {generateAnchor(<Inbox />, sendFilter, "archived", "Archived")}
-          </li>
+          {generateAnchor(
+            <AccountCircle />,
+            sendFilter,
+            "all",
+            "All Users",
+            state.selected.queryValue,
+            styles.selected
+          )}
+
+          {generateAnchor(
+            <Star />,
+            sendFilter,
+            "favorites",
+            "Favorites",
+            state.selected.queryValue,
+            styles.selected
+          )}
+
+          {generateAnchor(
+            <VerifiedUser />,
+            sendFilter,
+            "administrator",
+            "Administrator",
+            state.selected.queryValue,
+            styles.selected
+          )}
+
+          {generateAnchor(
+            <People />,
+            sendFilter,
+            "nonAdmins",
+            "None-Admins",
+            state.selected.queryValue,
+            styles.selected
+          )}
+
+          {generateAnchor(
+            <Inbox />,
+            sendFilter,
+            "archived",
+            "Archived",
+            state.selected.queryValue,
+            styles.selected
+          )}
         </ul>
       </div>
     </div>
